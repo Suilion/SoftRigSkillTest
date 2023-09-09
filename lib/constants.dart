25d56e9bf8ae6b08316cacea9db9c71d768ed152
Sully <Noragami50@gmail.com>
2023-09-09 14:13:02 +0200

    Created Homepage as well as the base for the view phonebook page and edit phonebook page

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ApiConstants {
+  static String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static String usersEndpoint = '/users';
+}
+
+class ColorConstants{
+  static Color DarkBlue = new Color(0xFF123F9A);
+  static Color LightBlue = new Color(0xFF2699FB);
+}
---
+++ lib/editPhonebook.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EditPhonebook extends StatelessWidget {
+  const EditPhonebook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Third Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
