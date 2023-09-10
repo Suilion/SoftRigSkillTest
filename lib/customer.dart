e2e18190f577c34bfab0db2750af97b277b480ac
Sully <Noragami50@gmail.com>
2023-09-10 14:39:15 +0200

    Using mock data, created a listview with expandable items in "view phonebook" page

---
+++ lib/customer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//Creating some test data
+
+final List customers = [
+  {
+    "Name": "Ola Nordman",
+    "Adress": "Olav Kyrre",
+    "Email": "CoolEmail@hotmail.com",
+    "Comment": "Nothing to note",
+  },
+  {
+    "Name": "Tuva Nordman",
+    "Adress": "Bergens Torget",
+    "Email": "CoolEmail@gmail.com",
+    "Comment": "A lot to note",
+  }
+];
--- lib/viewPhonebook.dart
+++ lib/viewPhonebook.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
+import 'customer.dart';
 
 class ViewPhonebook extends StatelessWidget {
   const ViewPhonebook({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('View Phonebook'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount:customers.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildExpandableTile(customers[index]);
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
   }
 }
+//This widget is what the list element will look like contracted and expanded
+Widget _buildExpandableTile(item) {
+  return ExpansionTile(
+    title: Text(
+      item['Name'],
+    ),
+    children: <Widget>[
+      ListTile(
+        title: Text(
+          (item['Adress'] + '\n' +  item['Email'] + '\n' +  item['Comment']),
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
+      )
+    ],
+  );
+}
+
